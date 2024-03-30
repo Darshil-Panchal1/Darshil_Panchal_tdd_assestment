6994fe328a3e28d2c609e254732121f31bd3147a
Darshil-Panchal <thedarshilpanchal@gmail.com>
2024-03-30 20:04:29 +0530

    Commit for: Step 2 - Allow the add method to handle an any amount of numbers and adding test cases related to it

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ function add(numbers) {
         return 0;
     }
     
-    const nums = numbers.split(',');
-    let total = 0;
-    for (const num of nums) {
-        total += parseInt(num);
-    }
-    
-    return total;
+    return numbers.split(',').reduce((total, num) => {
+        return total + parseInt(num);
+    }, 0);
 }
 
 module.exports = add;
@@ -17,3 +13,7 @@ module.exports = add;
 console.log(add(""));   
 console.log(add("1"));  
 console.log(add("1,5"));
+console.log(add("1,2,3,4,5"));
+console.log(add("10,20,30,40,50"));
+console.log(add("100,200,3000,400,555"));
+
--- test.js
+++ test.js
@@ -13,4 +13,16 @@ describe('add function', function() {
     it('should return the sum of numbers when input is comma-separated numbers string', function() {
         assert.strictEqual(add("1,5"), 6);
     });
+
+    it('should return the sum of numbers when input contains multipe numbers separated by commas', function() {
+        assert.strictEqual(add("1,2,3,4,5"), 15);
+    });
+
+    it('should return the sum of numbers when input contains multipe numbers with two digits separated by commas', function() {
+        assert.strictEqual(add("10,20,30,40,50"), 150);
+    });
+
+    it('should return the sum of numbers when input contains multipe numbers with three digits separated by commas', function() {
+        assert.strictEqual(add("100,200,3000,400,555"), 4255);
+    });
 });
