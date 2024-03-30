9895fca20d55a02579ea9b998f439777d4cca967
Darshil-Panchal <thedarshilpanchal@gmail.com>
2024-03-30 20:16:20 +0530

    Commit for: Step 3 - Allow the add method to handle new lines between numbers (instead of commas) and adding test cases for it

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function add(numbers) {
         return 0;
     }
     
+    numbers = numbers.replace(/\n/g, ',');
+
     return numbers.split(',').reduce((total, num) => {
         return total + parseInt(num);
     }, 0);
@@ -11,9 +13,12 @@ function add(numbers) {
 module.exports = add;
 
 console.log(add(""));   
-console.log(add("1"));  
-console.log(add("1,5"));
-console.log(add("1,2,3,4,5"));
-console.log(add("10,20,30,40,50"));
-console.log(add("100,200,3000,400,555"));
+console.log(add("1"));
+console.log(add("1\n5"));                
+console.log(add("1,2\n3,4\n5"));       
+console.log(add("10\n20,30\n40\n50"));
+console.log(add("1, \n"));
+console.log(add("1,\n"));
+
+
 
--- test.js
+++ test.js
@@ -10,19 +10,23 @@ describe('add function', function() {
         assert.strictEqual(add("1"), 1);
     });
 
-    it('should return the sum of numbers when input is comma-separated numbers string', function() {
-        assert.strictEqual(add("1,5"), 6);
+    it('should handle newline as delimiter and return the sum of numbers', function() {
+        assert.strictEqual(add("1\n5"), 6);
     });
 
-    it('should return the sum of numbers when input contains multipe numbers separated by commas', function() {
-        assert.strictEqual(add("1,2,3,4,5"), 15);
+    it('should handle newline and comma as delimiters and return the sum of numbers', function() {
+        assert.strictEqual(add("1\n2,3"), 6);
     });
 
-    it('should return the sum of numbers when input contains multipe numbers with two digits separated by commas', function() {
-        assert.strictEqual(add("10,20,30,40,50"), 150);
+    it('should handle newline and comma as delimiters, ignoring empty values, and return the sum of numbers', function() {
+        assert.strictEqual(add("1,2\n3,4\n5"), 15);
     });
 
-    it('should return the sum of numbers when input contains multipe numbers with three digits separated by commas', function() {
-        assert.strictEqual(add("100,200,3000,400,555"), 4255);
+    it('should handle newline and comma as delimiters, and return NaN if input contains invalid format ("1,\n")', function() {
+        assert.strictEqual(isNaN(add("1,\n")), true);
+    });
+
+    it('should handle whitespace around numbers and ignore them', function() {
+        assert.strictEqual(isNaN(add("1, \n")), true);
     });
 });
